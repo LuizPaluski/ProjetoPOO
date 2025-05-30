eabf34260ba4838e8c7d123314a0127b4fd5dc87
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-05-30 10:38:21 -0300

    Initial commit

---
+++ Animal.php
@@ -0,0 +1,26 @@
+<?php
+
+class Animal{
+    public string $nome;
+    public string $raca;
+    public int $patas;
+    public string $cor;
+    public float $peso;
+    public float $tamanho;
+
+    function Falar()
+    {
+        
+    }
+
+    function __construct()
+    {
+        $this->nome = "";
+        $this->raca = "";
+        $this->patas = 0;
+        $this->cor = "";
+        $this->peso = 0.0;
+        $this->tamanho = 0.0;
+    }
+
+}
---
+++ Cachorro.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'Animal.php';
+class Cachorro{
+    public int $auturalatido;
+
+    public function Falar($raca)
+    {
+        return "Au Au";
+    }
+}
