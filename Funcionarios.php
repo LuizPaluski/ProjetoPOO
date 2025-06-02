17a84d927c29834e5305b3aa8835a05b8a85ffc3
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-02 10:24:42 -0300

    Adicionado tela de venda e vendas

--- Funcionarios.php
+++ Funcionarios.php
@@ -1,58 +1,76 @@
 <?php
 require_once 'Humano.php';
+require_once "Vendas.php";
+require_once "index.php";
 
 class Funcionarios extends Humano
 {
     public string $cargo;
     public float $salario = 0.0;
+    public float $preco;
+    public string $produto;
+
+    public function Vender(): void
+    {
+        $this->produto = readline("Digite o produto;\n");
+        $this->preco = readline("Digite o Valor: \n");
+        echo "Produto: $this->produto, Preco: $this->preco\n";
+        $num = readline("Aperte 1 para sair:\n");
+        switch($num):
+            case 1:
+                TelaInicial();
+                endswitch;
+
+    }
 
     public function Pagamento(): void
     {
-        $salario = $this->salario ;
-        echo "Insira o codigo do cargo:\n";
-        echo "1. Vendedor. \n";
-        echo "2. Veterinario. \n";
-        echo "3. Gerente. \n";
-        echo "4. ADMIN \n";
-        $opcao = readline("digite o codigo do cargo: \n");
-        switch ($opcao):
-            case 1:
-                $senha = readline("Digite a senha:\n");
-                if($senha == 0000){
-                    echo "Vendedor salario de: ". $salario = 1518 ."\n";
-                }else{
-                    echo "Senha Invalida tente novamente...\n";
-                }
-                break;
-            case 2:
-                $senha = readline("Digite a senha:\n");
-                echo "Veterinario salario de: ". $salario = 3500 ."\n";
-                break;
-            case 3:
-                $senha = readline("Digite a senha:\n");
-                if($senha == 9999){
-                    echo "Gerente salario de: ". $salario = 6000 .  "\n";
-                }else{
-                    echo "Senha Invalida tente novamente...\n";
-                }
-                break;
-            case 4:
-                $senha = readline("Digite a senha:\n");
-                if($senha == 123){
-                    echo "ADMIN salario de: ".$salario = 1000000 . "\n";
-                }else {
-                    echo "Senha Invalida tente novamente...\n";
+            $salario = $this->salario;
+            echo "Insira o codigo do cargo:\n";
+            echo "1. Vendedor. \n";
+            echo "2. Veterinario. \n";
+            echo "3. Gerente. \n";
+            echo "4. ADMIN \n";
+            $opcao = readline("digite o codigo do cargo: \n");
+            switch ($opcao):
+                case 1:
+                    $senha = readline("Digite a senha:\n");
+                    if ($senha == 0000) {
+                        echo "Vendedor salario de: " . $salario = 1518 . "\n";
+                    } else {
+                        echo "Senha Invalida tente novamente...\n";
+                    }
+                    break;
+                case 2:
+                    $senha = readline("Digite a senha:\n");
+                    echo "Veterinario salario de: " . $salario = 3500 . "\n";
+                    break;
+                case 3:
+                    $senha = readline("Digite a senha:\n");
+                    if ($senha == 9999) {
+                        echo "Gerente salario de: " . $salario = 6000 . "\n";
+                    } else {
+                        echo "Senha Invalida tente novamente...\n";
+                    }
+                    break;
+                case 4:
+                    $senha = readline("Digite a senha:\n");
+                    if ($senha == 123) {
+                        echo "ADMIN salario de: " . $salario = 1000000 . "\n";
+                    } else {
+                        echo "Senha Invalida tente novamente...\n";
 
-                }
-            default;
-                echo "Opcao invalida: \n";
-        endswitch;
-
-    }
+                    }
+                default;
+                    echo "Opcao invalida: \n";
+            endswitch;
+            return;
+        }
 
     public function Trabalhar()
     {
-        echo "O funcionário está trabalhando.";
+        echo "O funcionário está trabalhando. \n";
     }
 }
+
 
--- index.php
+++ index.php
@@ -7,49 +7,55 @@ require_once "Cachorro.php";
 require_once "Raposa.php";
 require_once "Registros.php";
 
-while(true){
-    echo "Bem vindo ao servidor da LuizPets!\n";
-    echo "Escolha uma funcao:\n";
-    echo "1. Registrar Pessoa\n";
-    echo "2. Registrar Pets\n";
-    echo "3. Ouvir Animal\n";
-    echo "4. Sistema funcionarios: \n";
-    $opcao = readline("Digite a opcao: \n");
-    switch($opcao):
-        case 1:
-            $pessoa = new Humano();
-            RegistroPessoa($pessoa);
-            echo "Pessoa registrada com sucesso!\n";
-            break;
-        case 2:
-           if(!isset($pessoa) || !$pessoa instanceof Humano){ 
-                echo "Pessoa nao registrada ou invalida. Por favor, registre uma pessoa primeiro (Opcao 1).\n";
-            }else{
-                $novoAnimal = RegistarPets(); 
-                if ($novoAnimal instanceof Animal) { 
-                    $pessoa->AssociarPet($novoAnimal); 
-                    echo "Animal registrado e associado com sucesso a {$pessoa->nome}!\n"; 
+function TelaInicial(): void
+{
+        echo "Bem vindo ao servidor da LuizPets!\n";
+        echo "Escolha uma funcao:\n";
+        echo "1. Registrar Pessoa\n";
+        echo "2. Registrar Pets\n";
+        echo "3. Ouvir Animal\n";
+        echo "4. Sistema funcionarios: \n";
+        $opcao = readline("Digite a opcao: \n");
+        switch ($opcao):
+            case 1:
+                $pessoa = new Humano();
+                RegistroPessoa($pessoa);
+                echo "Pessoa registrada com sucesso!\n";
+                break;
+            case 2:
+                if (!isset($pessoa) || !$pessoa instanceof Humano) {
+                    echo "Pessoa nao registrada ou invalida. Por favor, registre uma pessoa primeiro (Opcao 1).\n";
+                } else {
+                    $novoAnimal = RegistarPets();
+                    if ($novoAnimal instanceof Animal) {
+                        $pessoa->AssociarPet($novoAnimal);
+                        echo "Animal registrado e associado com sucesso a {$pessoa->nome}!\n";
+                    }
                 }
-            }
-           break;
-        case 3:
-            if (isset($pessoa) && $pessoa instanceof Humano && $pessoa->pet instanceof Animal) {
-                echo "O pet de". " {$pessoa->nome}, {$pessoa->pet->nome}, faz: ". "{$pessoa->pet->Falar()}\n";
-            } else {
-                echo "Nenhuma pessoa com pet registrado encontrada. Registre uma pessoa e um pet primeiro.\n";
-            }
-            break;
-        case 4:
-            $senhafuncionario  = readline("Digite a senha: ");
-            if($senhafuncionario == "naotemsenha"){
-                $funcionario = new Funcionarios();
-                echo $funcionario->Pagamento(). $funcionario->Trabalhar();
-            }else {
-                echo "Senha incorreta. Tente novamente! \n";
-            }
-        default:
-            echo "Opcao invalida. Tente novamente!.\n";
-            break;
-    endswitch;
-        }
-            
+                break;
+            case 3:
+                if (isset($pessoa) && $pessoa instanceof Humano && $pessoa->pet instanceof Animal) {
+                    echo "O pet de" . " {$pessoa->nome}, {$pessoa->pet->nome}, faz: " . "{$pessoa->pet->Falar()}\n";
+                } else {
+                    echo "Nenhuma pessoa com pet registrado encontrada. Registre uma pessoa e um pet primeiro.\n";
+                }
+                break;
+            case 4:
+                $senhafuncionario = readline("Digite a senha: ");
+                if ($senhafuncionario == "naotemsenha") {
+                    $funcionario = new Funcionarios();
+                    echo $funcionario->Pagamento() . $funcionario->Trabalhar() . $funcionario->Vender();
+                    while (true) {
+                        $funcionario->Vender();
+                    }
+                }
+            default:
+                echo "Opcao invalida. Tente novamente!.\n";
+                break;
+        endswitch;
+
+}
+
+while(true){
+    TelaInicial();
+}
