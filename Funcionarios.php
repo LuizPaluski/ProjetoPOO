7d4e9c3a23887bc9edf24985324586067d5188f4
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-06-02 09:18:47 -0300

    Adicionado opcao de entrar como funcionario

--- Funcionarios.php
+++ Funcionarios.php
@@ -1,19 +1,58 @@
 <?php
-/*require_once 'Humano.php';
+require_once 'Humano.php';
 
-class Funcionario extends Humano
+class Funcionarios extends Humano
 {
     public string $cargo;
-    public float $salario;
+    public float $salario = 0.0;
 
-    public function __construct()
+    public function Pagamento(): void
     {
-        $this->cargo = "";
-        $this->salario = 0.0;
+        $salario = $this->salario ;
+        echo "Insira o codigo do cargo:\n";
+        echo "1. Vendedor. \n";
+        echo "2. Veterinario. \n";
+        echo "3. Gerente. \n";
+        echo "4. ADMIN \n";
+        $opcao = readline("digite o codigo do cargo: \n");
+        switch ($opcao):
+            case 1:
+                $senha = readline("Digite a senha:\n");
+                if($senha == 0000){
+                    echo "Vendedor salario de: ". $salario = 1518 ."\n";
+                }else{
+                    echo "Senha Invalida tente novamente...\n";
+                }
+                break;
+            case 2:
+                $senha = readline("Digite a senha:\n");
+                echo "Veterinario salario de: ". $salario = 3500 ."\n";
+                break;
+            case 3:
+                $senha = readline("Digite a senha:\n");
+                if($senha == 9999){
+                    echo "Gerente salario de: ". $salario = 6000 .  "\n";
+                }else{
+                    echo "Senha Invalida tente novamente...\n";
+                }
+                break;
+            case 4:
+                $senha = readline("Digite a senha:\n");
+                if($senha == 123){
+                    echo "ADMIN salario de: ".$salario = 1000000 . "\n";
+                }else {
+                    echo "Senha Invalida tente novamente...\n";
+
+                }
+            default;
+                echo "Opcao invalida: \n";
+        endswitch;
+
     }
 
     public function Trabalhar()
     {
-        return "O funcionário está trabalhando.";
+        echo "O funcionário está trabalhando.";
     }
 }
+
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once "Animal.php";
 require_once "Humano.php";
+require_once "Funcionarios.php";
 require_once "Gato.php";
 require_once "Cachorro.php";
 require_once "Raposa.php";
@@ -11,6 +12,8 @@ while(true){
     echo "Escolha uma funcao:\n";
     echo "1. Registrar Pessoa\n";
     echo "2. Registrar Pets\n";
+    echo "3. Ouvir Animal\n";
+    echo "4. Sistema funcionarios: \n";
     $opcao = readline("Digite a opcao: \n");
     switch($opcao):
         case 1:
@@ -28,6 +31,7 @@ while(true){
                     echo "Animal registrado e associado com sucesso a {$pessoa->nome}!\n"; 
                 }
             }
+           break;
         case 3:
             if (isset($pessoa) && $pessoa instanceof Humano && $pessoa->pet instanceof Animal) {
                 echo "O pet de". " {$pessoa->nome}, {$pessoa->pet->nome}, faz: ". "{$pessoa->pet->Falar()}\n";
@@ -35,8 +39,16 @@ while(true){
                 echo "Nenhuma pessoa com pet registrado encontrada. Registre uma pessoa e um pet primeiro.\n";
             }
             break;
+        case 4:
+            $senhafuncionario  = readline("Digite a senha: ");
+            if($senhafuncionario == "naotemsenha"){
+                $funcionario = new Funcionarios();
+                echo $funcionario->Pagamento(). $funcionario->Trabalhar();
+            }else {
+                echo "Senha incorreta. Tente novamente! \n";
+            }
         default:
-            echo "Opcao invalida. Tente novamente.\n";
+            echo "Opcao invalida. Tente novamente!.\n";
             break;
     endswitch;
         }
